a3b87d67648018f6d6cbeee602192640c4eb3a58
moroccan-coder <tssryassine@gmail.com>
2020-10-05 01:30:35 -0700

    adding Popular tabed Screen

--- lib/screens/home_tabs/popular.dart
+++ lib/screens/home_tabs/popular.dart
@@ -8,8 +8,65 @@ class Popular extends StatefulWidget {
 class _PopularState extends State<Popular> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return Card(
+          child: _drawSingleRow(),
+        );
+      },
+    );
+  }
+
+  Widget _drawSingleRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 90,
+            width: 90,
+            child: Image(
+              image: ExactAssetImage('assets/images/placeholder_bg.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            width: 18,
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "At vero eos et accusamus et iusto odio",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('yassine el haitar'),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.timer,
+                          size: 16,
+                        ),
+                        Text(
+                          '10 min ago',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
