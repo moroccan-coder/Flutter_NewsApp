21f86bb0ac09377e31bea96dbb136d778ed9e328
moroccan-coder <tssryassine@gmail.com>
2020-10-16 06:59:06 -0700

    Recent update - fetch data from Api

--- lib/screens/home_tabs/whats_new.dart
+++ lib/screens/home_tabs/whats_new.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:news_app/api/posts_api.dart';
 import 'package:news_app/models/post.dart';
 import 'package:timeago/timeago.dart' as timeago;
+
 class WhatsNew extends StatefulWidget {
   @override
   _WhatsNewState createState() => _WhatsNewState();
 }
 
 class _WhatsNewState extends State<WhatsNew> {
-
   PostsAPI postApi = new PostsAPI();
 
   @override
@@ -84,20 +84,54 @@ class _WhatsNewState extends State<WhatsNew> {
             child: Card(
               child: FutureBuilder(
                 future: postApi.fetchWhatsNew(),
-                builder: (context,AsyncSnapshot snapshot) {
+                builder: (context, AsyncSnapshot snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return _loading();
+                      break;
 
-                  Post post1 = snapshot.data[0];
-                  Post post2 = snapshot.data[1];
-                  Post post3 = snapshot.data[2];
+                    case ConnectionState.active:
+                      return _loading();
+                      break;
 
-                  return Column(
-                    children: [
-                      _drawSingleRow(post1),
-                      _drawDivider(),
-                      _drawSingleRow(post2),
-                      _drawDivider(),
-                      _drawSingleRow(post3),
-                    ],
+                    case ConnectionState.none:
+                      return _connectionError();
+                      break;
+
+                    case ConnectionState.done:
+                      if (snapshot.error != null) {
+                        return _error(snapshot.error);
+                      } else {
+                        if (snapshot.hasData) {
+                          List<Post> posts = snapshot.data;
+
+                          if (posts.length >= 3) {
+                            Post post1 = snapshot.data[0];
+                            Post post2 = snapshot.data[1];
+                            Post post3 = snapshot.data[2];
+
+                            return Column(
+                              children: [
+                                _drawSingleRow(post1),
+                                _drawDivider(),
+                                _drawSingleRow(post2),
+                                _drawDivider(),
+                                _drawSingleRow(post3),
+                              ],
+                            );
+                          } else {
+                            return _noData();
+                          }
+                        } else {
+                          return _noData();
+                        }
+                      }
+                      break;
+                  }
+
+                  return Container(
+                    height: 0,
+                    width: 0,
                   );
                 },
               ),
@@ -116,7 +150,10 @@ class _WhatsNewState extends State<WhatsNew> {
           SizedBox(
             height: 90,
             width: 90,
-            child: Image.network(post.featuredImage,fit: BoxFit.cover,),
+            child: Image.network(
+              post.featuredImage,
+              fit: BoxFit.cover,
+            ),
           ),
           SizedBox(
             width: 18,
@@ -134,7 +171,10 @@ class _WhatsNewState extends State<WhatsNew> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('yassine el haitar',style: TextStyle(fontSize: 12),),
+                    Text(
+                      'yassine el haitar',
+                      style: TextStyle(fontSize: 12),
+                    ),
                     Row(
                       children: [
                         Icon(
@@ -174,32 +214,59 @@ class _WhatsNewState extends State<WhatsNew> {
     );
   }
 
-
-
-
-
-  Widget _drawRecentUpdates()
-  {
-    return  Padding(
+  Widget _drawRecentUpdates() {
+    return Padding(
       padding: EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8, left: 8),
-            child: _drawSectionTitle("Recent Updates"),
-          ),
-          _DrawRecentUpdatesCard(Colors.red),
-          _DrawRecentUpdatesCard(Colors.green),
-          SizedBox(
-            height: 40,
-          ),
-        ],
+      child: FutureBuilder(
+        future: postApi.fetchRecentUpdate(),
+        builder: (context,AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return _connectionError();
+              break;
+
+            case ConnectionState.active:
+              return _loading();
+              break;
+
+            case ConnectionState.waiting:
+              return _loading();
+              break;
+
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return _error(snapshot.error);
+              } else {
+                if (snapshot.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8, left: 8),
+                        child: _drawSectionTitle("Recent Updates"),
+                      ),
+                      _DrawRecentUpdatesCard(Colors.red,snapshot.data[0]),
+                      _DrawRecentUpdatesCard(Colors.green,snapshot.data[1]),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  );
+                } else {
+                  return _noData();
+                }
+              }
+
+              break;
+          }
+
+          return Container(width: 0,height: 0,);
+        },
       ),
     );
   }
 
-  Widget _DrawRecentUpdatesCard(Color color) {
+  Widget _DrawRecentUpdatesCard(Color color, Post post) {
     return Card(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -207,7 +274,7 @@ class _WhatsNewState extends State<WhatsNew> {
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: ExactAssetImage("assets/images/placeholder_bg.png"),
+                image: NetworkImage(post.featuredImage),
                 fit: BoxFit.cover,
               ),
             ),
@@ -221,19 +288,22 @@ class _WhatsNewState extends State<WhatsNew> {
               padding: EdgeInsets.only(top: 2, bottom: 2),
               child: Text(
                 "Sport",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800),
                 textAlign: TextAlign.center,
               ),
               decoration: BoxDecoration(
-                  color: color, borderRadius: BorderRadius.circular(4)),
+                  color: color,
+                  borderRadius: BorderRadius.circular(4)),
             ),
           ),
           Padding(
             padding: EdgeInsets.all(8),
             child: Text(
-              "De Finibus Bonorum et Malorum",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              post.title,
+              style: TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.w600),
             ),
           ),
           Padding(
@@ -247,20 +317,50 @@ class _WhatsNewState extends State<WhatsNew> {
                 SizedBox(
                   width: 4,
                 ),
-                Text("16 min"),
+                Text(_parseDate(post.dateWritten)),
               ],
             ),
           )
         ],
+      )
+    );
+  }
+
+  String _parseDate(String dateTime) {
+    DateTime time = DateTime.parse(dateTime);
+
+    return timeago.format(time);
+  }
+
+  //Loading ProgressBar TopStories Item
+
+  Widget _loading() {
+    return Container(
+      padding: EdgeInsets.all(40),
+      child: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
 
+  Widget _connectionError() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Text("Connection Error!!!"),
+    );
+  }
 
-  String _parseDate(String dateTime)
-  {
-   DateTime time = DateTime.parse(dateTime);
+  Widget _error(var error) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Text(error.toString()),
+    );
+  }
 
-    return timeago.format(time);
+  Widget _noData() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Text("No Data Available!"),
+    );
   }
 }
--- lib/utilities/api_utilities.dart
+++ lib/utilities/api_utilities.dart
@@ -3,4 +3,5 @@ String base_api = "https://newsapp.coders.ma";
 String all_authors_api ="/public/api/authors";
 String all_categories_api = '/public/api/categories';
 String whats_new_api = '/public/api/posts/category/1';
+String recent_update_api = '/public/api/posts/category/2';
 
