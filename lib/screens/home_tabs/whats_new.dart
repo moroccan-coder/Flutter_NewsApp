d12b6a5ef55232fc4432699648b86b976d8fe701
moroccan-coder <tssryassine@gmail.com>
2020-10-16 08:32:39 -0700

    whats new - random header post

--- lib/screens/home_tabs/whats_new.dart
+++ lib/screens/home_tabs/whats_new.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/api/posts_api.dart';
@@ -38,34 +40,82 @@ class _WhatsNewState extends State<WhatsNew> {
       fontSize: 14,
     );
 
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.26,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: ExactAssetImage('assets/images/placeholder_bg.png'),
-        fit: BoxFit.cover,
-      )),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 40, right: 40),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              'The standard & Lorem Ipsum passage.',
-              style: _headerTitle,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit in a consectetur',
-              style: _headerDescription,
-              textAlign: TextAlign.center,
-            ),
-          ]),
-        ),
-      ),
+    return FutureBuilder(
+      future: postApi.fetchPostsByCategory("9"),
+      builder: (context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return connectionError();
+            break;
+
+          case ConnectionState.active:
+            return loading();
+            break;
+          case ConnectionState.waiting:
+            return loading();
+            break;
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return error(snapshot.error);
+            } else {
+              if (snapshot.hasData) {
+                List<Post> posts = snapshot.data;
+                Random random = Random();
+                int randomIndex = random.nextInt(posts.length);
+                Post post = posts[randomIndex];
+
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.26,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                    image: NetworkImage(post.featuredImage),
+                    fit: BoxFit.cover,
+                  )),
+                  child: Stack(
+                    children: [
+                      Container(
+                        color: Colors.black.withOpacity(0.4),
+                      )
+                      ,
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 40, right: 40),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  post.title,
+                                  style: _headerTitle,
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Text(
+                                  post.content.substring(0, 150),
+                                  style: _headerDescription,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return noData();
+              }
+            }
+
+            break;
+        }
+
+        return Container(
+          width: 0,
+          height: 0,
+        );
+      },
     );
   }
 
@@ -219,7 +269,7 @@ class _WhatsNewState extends State<WhatsNew> {
       padding: EdgeInsets.all(8.0),
       child: FutureBuilder(
         future: postApi.fetchPostsByCategory("4"),
-        builder: (context,AsyncSnapshot snapshot) {
+        builder: (context, AsyncSnapshot snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
               return connectionError();
@@ -245,8 +295,8 @@ class _WhatsNewState extends State<WhatsNew> {
                         padding: const EdgeInsets.only(bottom: 8, left: 8),
                         child: _drawSectionTitle("Recent Updates"),
                       ),
-                      _DrawRecentUpdatesCard(Colors.red,snapshot.data[0]),
-                      _DrawRecentUpdatesCard(Colors.green,snapshot.data[1]),
+                      _DrawRecentUpdatesCard(Colors.red, snapshot.data[0]),
+                      _DrawRecentUpdatesCard(Colors.green, snapshot.data[1]),
                       SizedBox(
                         height: 40,
                       ),
@@ -260,7 +310,10 @@ class _WhatsNewState extends State<WhatsNew> {
               break;
           }
 
-          return Container(width: 0,height: 0,);
+          return Container(
+            width: 0,
+            height: 0,
+          );
         },
       ),
     );
@@ -268,64 +321,57 @@ class _WhatsNewState extends State<WhatsNew> {
 
   Widget _DrawRecentUpdatesCard(Color color, Post post) {
     return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(post.featuredImage),
-                fit: BoxFit.cover,
-              ),
-            ),
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.26,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 90,
-              padding: EdgeInsets.only(top: 2, bottom: 2),
-              child: Text(
-                "Sport",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w800),
-                textAlign: TextAlign.center,
-              ),
-              decoration: BoxDecoration(
-                  color: color,
-                  borderRadius: BorderRadius.circular(4)),
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(post.featuredImage),
+              fit: BoxFit.cover,
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(8),
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.26,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 90,
+            padding: EdgeInsets.only(top: 2, bottom: 2),
             child: Text(
-              post.title,
-              style: TextStyle(
-                  fontSize: 18, fontWeight: FontWeight.w600),
+              "Sport",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+              textAlign: TextAlign.center,
             ),
+            decoration: BoxDecoration(
+                color: color, borderRadius: BorderRadius.circular(4)),
           ),
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.access_time,
-                  size: 18,
-                ),
-                SizedBox(
-                  width: 4,
-                ),
-                Text(parseDateHuman(post.dateWritten)),
-              ],
-            ),
-          )
-        ],
-      )
-    );
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            post.title,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Icon(
+                Icons.access_time,
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text(parseDateHuman(post.dateWritten)),
+            ],
+          ),
+        )
+      ],
+    ));
   }
-
-
-
 }
