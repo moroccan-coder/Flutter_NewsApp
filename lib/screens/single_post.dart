4a6937d5f3e8e1c863daee401b3e897ab998c8bd
moroccan-coder <tssryassine@gmail.com>
2020-10-17 03:41:08 -0700

    abb sliver appBar an SliverList

--- lib/screens/single_post.dart
+++ lib/screens/single_post.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:news_app/models/post.dart';
 
 class SinglePost extends StatefulWidget {
-
   final Post post;
+
   SinglePost(this.post);
 
   @override
@@ -11,14 +13,46 @@ class SinglePost extends StatefulWidget {
 }
 
 class _SinglePostState extends State<SinglePost> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.black38,
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 300.0,
+          floating: true,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+
+              background: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(widget.post.featuredImage),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+          (context, index) => Container(
+            color: getRandomColor(),
+            height: 150.0,
+          ),
+        )),
+      ],
+    );
+  }
+
+  final random = Random();
+
+  /// Returns a random color, with minumim brightness level
+  Color getRandomColor({int minBrightness = 50}) {
+    assert(minBrightness >= 0 && minBrightness <= 255);
+    return Color.fromARGB(
+      0xFF,
+      minBrightness + random.nextInt(255 - minBrightness),
+      minBrightness + random.nextInt(255 - minBrightness),
+      minBrightness + random.nextInt(255 - minBrightness),
     );
   }
 }
